52a7b37c1b6613c8203224f603fada23d66e10fd
Mustafa Mohamed <mmohamed4@academic.rrc.ca>
2025-05-15 21:07:39 -0500

    Adding last touches.

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 """This Module 01 Assignment for COMP-1327 Software 
-Development Fundamentals and will be used to practice my Git commands and GitHub workflows."""
+Development Fundamentals will be used to practice and explain my Git commands and GitHub workflows."""
 
 __author__ = "Mustafa Mohamed"
 __version__ = "1.0"
