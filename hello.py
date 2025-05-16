c3c0247ccb330b9eb282895a17407fe1949f31a1
Mustafa Mohamed <mmohamed4@academic.rrc.ca>
2025-05-15 19:12:50 -0500

    Initial commit: Set up project directory and file structure.

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-"""This module serves as part of Module 01 Assignment for COMP-1327 Software Development Fundamentals.
-It will be used to practice Git commands and GitHub workflows."""
+"""This module serves as part of Module 01 Assignment for COMP-1327 Software 
+Development Fundamentals. It will be used to practice Git commands and GitHub workflows."""
 
 __author__ = "Mustafa Mohamed"
 __version__ = "1.0"
